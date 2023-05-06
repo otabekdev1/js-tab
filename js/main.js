c6d4dff9ad24f737408271f712c649da2d5650eb
otabekdev1 <githubisomovotabek@gmail.com>
2023-05-06 17:19:32 +0500

    working

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const tabs = document.querySelectorAll(".tab_btn");
+const content = document.querySelectorAll(".content");
+tabs.forEach((tab, index) => {
+  tab.addEventListener("click", (e) => {
+    tabs.forEach((tab) => tab.classList.remove("active"));
+    tab.classList.add("active");
+    var line = document.querySelector(".line");
+    line.style.width = e.target.offsetWidth + "px";
+    line.style.left = e.target.offsetLeft + "px";
+    content.forEach((content) => {
+      content.classList.remove("active");
+    });
+    content[index].classList.add("active");
+  });
+});
